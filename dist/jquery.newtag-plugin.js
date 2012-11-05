90fb8991bf232799bc0cb7a7a0cdffcf23f54d78
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2012-11-04 19:29:02 -0700

    Added comments.

--- dist/jquery.newtag-plugin.js
+++ dist/jquery.newtag-plugin.js
@@ -1,60 +1,62 @@
-/*! jQuery Newtag Plugin - v0.1.0 - 2012-11-02
+/*! jQuery Newtag Plugin - v0.1.0 - 2012-11-04
 * https://github.com/jonahlyn/newtag-plugin
 * Author: Jonahlyn Gilstrap;*/
 
-(function($){
-  
-    $.fn.newtag = function(options) {
-        
-        var opts = $.extend({}, $.fn.newtag.defaults, options),
-            today = new Date();
-        
-        return this.each(function(){
-            
-            var $el = $(this),
-                expiration = new Date(opts.expiration),
-                newicon;
-                
-            if (today <= expiration) {
-              newicon = $('<span>').addClass(opts.classname).text(opts.text);
-              newicon.css(opts.css);
-              
-              $el.append(newicon);
-              
-              if(opts.parentcss){
-                newicon.parent().css(opts.parentcss);
-              }
+( function($) {
+
+        $.fn.newtag = function(options) {
+
+            var opts = $.extend({}, $.fn.newtag.defaults, options), today = new Date();
+
+            return this.each(function() {
+
+                var $el = $(this), expiration = new Date(opts.expiration), newicon;
+
+                // check that today is before the expiration date
+                if (today <= expiration) {
+                    // create a new span with specified text and class
+                    newicon = $('<span>').addClass(opts.classname).text(opts.text);
+                    newicon.css(opts.css);
+                    
+                    // add new span to selected element
+                    $el.append(newicon);
+
+                    // add css to parent element
+                    if (opts.parentcss) {
+                        newicon.parent().css(opts.parentcss);
+                    }
+                }
+            });
+
+        };
+
+        $.fn.newtag.defaults = {
+            'expiration' : '2012/12/31',
+            'classname' : 'new',
+            'text' : 'new!',
+            'css' : {
+                'color' : 'gold',
+                'font-size' : '14px',
+                'padding-left' : '5px',
+                'position' : 'absolute',
+                'top' : '5px',
+                'right' : '10px',
+                '-webkit-transform' : 'rotate(20deg)',
+                '-moz-transform' : 'rotate(20deg)',
+                '-ms-transform' : 'rotate(20deg)',
+                '-o-transform' : 'rotate(20deg)',
+                'transform' : 'rotate(20deg)',
+                '-webkit-transform-origin' : 'bottom right',
+                '-moz-transform-origin' : 'bottom right',
+                '-ms-transform-origin' : 'bottom right',
+                '-o-transform-origin' : 'bottom right',
+                'transform-origin' : 'bottom right',
+                '-webkit-backface-visibility' : 'hidden',
+                'text-shadow' : '1px 1px 1px #1F1E1E'
+            },
+            'parentcss' : {
+                'position' : 'relative'
             }
-        });
-        
-    };
-    
-    $.fn.newtag.defaults = {
-        'expiration' : '2012/12/31', 
-        'classname' : 'new',
-        'text' : 'new!',
-        'css' : {
-          'color': 'gold',
-          'font-size': '14px',
-          'padding-left':'5px',
-          'position':'absolute',
-          'top':'5px',
-          'right':'10px',
-          '-webkit-transform':'rotate(20deg)',
-          '-moz-transform':'rotate(20deg)',
-          '-ms-transform':'rotate(20deg)',
-          '-o-transform':'rotate(20deg)',
-          'transform':'rotate(20deg)',
-          '-webkit-transform-origin':'bottom right',
-          '-moz-transform-origin':'bottom right',
-          '-ms-transform-origin':'bottom right',
-          '-o-transform-origin':'bottom right',
-          'transform-origin':'bottom right',
-          '-webkit-backface-visibility':'hidden',
-          'text-shadow':'1px 1px 1px #1F1E1E'
-        },
-        'parentcss': {'position':'relative'}
-    };
-    
-    
-}(jQuery));
+        };
+
+    }(jQuery));
